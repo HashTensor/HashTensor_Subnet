95ebaa2487147f6edaa2302620574a5bc0e479f1
k.panchenko <k.panchenko@simcord.com>
2025-06-05 13:54:07 +0200

    Update CORS middleware configuration in main.py for environment-specific origins
    - Modified the CORS middleware to allow all origins in the test environment while restricting it to a specific remote site origin in production.
    - This change enhances security by preventing unrestricted access in non-test environments.

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ app = FastAPI(prefix="/api", title="HashTensor Validator", lifespan=lifespan)
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"] if ENV == "test" else [REMOTE_SITE_ORIGIN],  # Not recommended for production!
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
