cb0098dbd09096eacfea12ea3167a098af807cd6
k.panchenko <k.panchenko@simcord.com>
2025-06-15 20:57:45 +0200

    Update __init__.py to include previous specification version constant
    - Added a constant for PREV_SPEC_VERSION to track the previous specification version.
    - Adjusted the calculation of __spec_version__ to incorporate the new constant for improved versioning clarity.

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,7 @@
 from importlib.metadata import version, PackageNotFoundError
 
+PREV_SPEC_VERSION = 419000
+
 try:
     __version__ = version("hashtensor-validator")
 except PackageNotFoundError:
@@ -11,4 +13,4 @@ __spec_version__ = (
     (1000 * int(version_split[0]))
     + (10 * int(version_split[1]))
     + (1 * int(version_split[2]))
-)
+) + PREV_SPEC_VERSION
